d331868c9a91b8d403a44fd2cf51f90331a8781f
EnderEyeGames <43973448+EnderEyeGames@users.noreply.github.com>
2021-05-09 01:02:15 -0500

    Clarify requirements
    This clarifies that 1500 kbps refers to x264 medium, 150 kbps audio refers to AAC low complexity, and that timestamps are disallowed. It also makes clear that you can't just resize a video or pad it.

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on("ready", () =>{
                 data: {
                     type: 4,
                     data: {
-                        content: "To compress size so you send on discord you can use: https://8mb.video/ \n enable  `Extra quality (slower)` in options for best quality"
+                        content: "To compress size so you send on discord you can use: https://8mb.video/ \nYou must enable `Extra quality (slower)` in options if longer than 20 seconds."
                     }
                 }
             })
@@ -80,7 +80,7 @@ client.on("ready", () =>{
             .setTitle('Requirements')
             .setAuthor('Gamers React', 'https://cdn.discordapp.com/emojis/764541981560537110.png?v=1')
             .setColor(0xff0000)
-            .setDescription('All submissions must meet the following requirements:\n> Video resolution: At least 1280x720\n> Aspect ratio: Anything between 16:10 and 2:1\n> Framerate: At least 30 fps\n> Video bitrate: At least 1500 Kbps\n> Audio bitrate: At least 150 Kbps\n> Must be viewable from discord\n> Youtube video: must be under 2 min')
+            .setDescription('All submissions must meet the following requirements:\n> Video resolution: At least 1280x720\n> Aspect ratio: Anything between 16:10 and 2:1\n> Framerate: At least 30 fps\n> Video bitrate: At least 1500 kbps (x264 medium)\n> Audio bitrate: At least 150 kbps (AAC-LC)\n> Must embed on discord\n> Must be under 2 minutess. No timestamps!\nDeliberately scaling or padding a video to fool me\ndoes **not** pass the requirements.')
 
             client.api.interactions(interaction.id, interaction.token).callback.post({
                 data: {
