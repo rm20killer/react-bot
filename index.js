8917a1e7f5eacd1dfdd6d83df99fbefc7afb0a5f
EnderEyeGames <43973448+EnderEyeGames@users.noreply.github.com>
2021-05-31 13:25:27 -0500

    Update /compress
    40 seconds with x264 medium just barely meets the requirements, so /compress now clarifies that.

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on("ready", () =>{
                 data: {
                     type: 4,
                     data: {
-                        content: "To compress size so you send on discord you can use: https://8mb.video/ \n **You must** enable `Extra quality (slower)` in options if longer than 20 seconds."
+                        content: "To compress size so you send on discord you can use: https://8mb.video/ \n **You must** enable the `Extra quality (slower)` option.\nYour video cannot be longer than 40 seconds to meet requirements.\nUse the trim options to accomplish this."
                     }
                 }
             })
