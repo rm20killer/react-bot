efbf68df42c927fcc7f84f8d6f7c754a44c9aac7
RM20 <43253883+rm20killer@users.noreply.github.com>
2021-06-13 20:31:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const { Channel } = require("discord.js");
 
 module.exports ={
     //Discord bot token
-    BotToken:"Nzc0NjA0ODUyNzU0OTA3MTU2.X6aM_A.M0w1u6al6zn7sXZv6bYod1M3Tuc",
+    BotToken:"",
     // youtube tokens
-    youtubeKey: "AIzaSyCV9Jr1-bo9BufRdRuHI_Uu_XIkc8FM5eI",
-    youtubeUser: "UCvInsdoSCTRGQNuXe7kMjhQ",
+    youtubeKey: "",
+    youtubeUser: "",
     //The thing before a command to call the bot
     prefix:'*',
 
