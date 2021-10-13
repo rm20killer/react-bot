b91a4adc2d52c754c1c05142525052ffb8eba84f
RM20 <43253883+rm20killer@users.noreply.github.com>
2021-10-13 12:20:12 +0100

    spreedsheet intergration

---
+++ commands/spreadsheet/spreadsheet.js
@@ -0,0 +1,29 @@
+const fetch = require("node-fetch");
+const Discord = require('discord.js')
+const { Client, Intents } = require('discord.js');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+const client = new Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING
+    ],
+});
+
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+
+const config = require("../../config");
+const creds = require("./googlekey.json");
+const doc = new GoogleSpreadsheet(config.spreadsheet);
