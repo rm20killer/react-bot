710df3d6904de70f0c6353f6f8890377b6659195
RM20 <43253883+rm20killer@users.noreply.github.com>
2021-11-13 23:52:44 +0000

    similarity command

--- commands/cmd.js
+++ commands/cmd.js
@@ -113,6 +113,16 @@ module.exports ={
             }
         }
         if (message.member.roles.cache.find(r=>r.name === modid)||message.member.roles.cache.find(r=>r.name === adminid)||message.member.roles.cache.find(r=>r.id === helper)){
+            if(cmd==="similarity"){
+                if(args[1]===null){
+                    return
+                }
+                
+                if(args[2]===null){
+                    return
+                }
+                message.reply(""+similarity(args[1],args[2]))
+            }
             if(cmd==="masscheck"){
                 //namechecker.masscheck(message,client,args)
             }
@@ -346,4 +356,46 @@ const setactivity  = async (message,client,type,str) => {
     console.log(type + actt);
     client.user.setActivity(actt, { type: type});
     message.reply("updated Activity to "+ type +" "+actt);
+}
+
+
+function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+      longer = s2;
+      shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+      return 1.0;
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function editDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    var costs = new Array();
+    for (var i = 0; i <= s1.length; i++) {
+      var lastValue = i;
+      for (var j = 0; j <= s2.length; j++) {
+        if (i == 0)
+          costs[j] = j;
+        else {
+          if (j > 0) {
+            var newValue = costs[j - 1];
+            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+              newValue = Math.min(Math.min(newValue, lastValue),
+                costs[j]) + 1;
+            costs[j - 1] = lastValue;
+            lastValue = newValue;
+          }
+        }
+      }
+      if (i > 0)
+        costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
 }
