6ade5f2e93f950b9811f93d94b62b5a754abe3bb
RM20 <43253883+rm20killer@users.noreply.github.com>
2022-04-19 23:18:17 +0100

    NEED TO REMOVE AFTER

--- index.js
+++ index.js
@@ -389,3 +389,19 @@ client.giveawaysManager.on('giveawayDeleted', (giveaway) => {
     channel.send('Giveaway with message Id ' + giveaway.messageId + ' was deleted.')
 });
 
+client.on('voiceStateUpdate', (oldState, newState) => {
+    console.log("user joined vc")
+    const txtChannel = client.channels.cache.get('698585110886481960'); //manually input your own channel
+    const newChannelID = newState.channelId;
+    const oldChannelID = oldState.channelId;
+    //console.log(oldState)
+    if (oldChannelID === "629695220065239066") { //manually put the voice channel ID
+        txtChannel.send('role removed');
+        let role = newState.guild.roles.cache.get("966097737823178762"); //added this
+        newState.member.roles.remove(role).catch(console.error);
+    } else if (newChannelID === "629695220065239066") {
+        txtChannel.send('role given');
+        let role = oldState.guild.roles.cache.get("966097737823178762"); //change this somewhat
+        oldState.member.roles.add(role).catch(console.error); //adding a catch method is always good practice
+    }
+})
