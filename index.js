831d58fcf783fe76cb4859bf8afea7c071aa456d
RM20 <43253883+rm20killer@users.noreply.github.com>
2022-04-19 23:24:34 +0100

    moved chat messages

--- index.js
+++ index.js
@@ -391,16 +391,16 @@ client.giveawaysManager.on('giveawayDeleted', (giveaway) => {
 
 client.on('voiceStateUpdate', (oldState, newState) => {
     console.log("user joined vc")
-    const txtChannel = client.channels.cache.get('698585110886481960'); //manually input your own channel
+    const txtChannel = client.channels.cache.get('966101775226634340'); //manually input your own channel
     const newChannelID = newState.channelId;
     const oldChannelID = oldState.channelId;
     //console.log(oldState)
     if (oldChannelID === "629695220065239066") { //manually put the voice channel ID
-        txtChannel.send('role removed');
+        txtChannel.send(`role removed - ${newState.id}`);
         let role = newState.guild.roles.cache.get("966097737823178762"); //added this
         newState.member.roles.remove(role).catch(console.error);
     } else if (newChannelID === "629695220065239066") {
-        txtChannel.send('role given');
+        txtChannel.send(`role given - ${newState.id}`);
         let role = oldState.guild.roles.cache.get("966097737823178762"); //change this somewhat
         oldState.member.roles.add(role).catch(console.error); //adding a catch method is always good practice
     }
