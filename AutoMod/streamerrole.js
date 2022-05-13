f1a061c517d782f0535f49e12e5e4f415d543ed3
codefactor-io <support@codefactor.io>
2022-05-13 00:29:47 +0000

    [CodeFactor] Apply fixes to commit abfebe2

--- AutoMod/streamerrole.js
+++ AutoMod/streamerrole.js
@@ -5,7 +5,7 @@ const { Client, Intents } = require('discord.js');
 module.exports = {
     streamerrole: function (messa, message, client) {
         const sregex = /(how|where|want).+(streamer|content creator|youtuber).+(role|rank)/i;
-        const sstr = messa;;
+        const sstr = messa;
         if ((x = sregex.exec(sstr)) !== null) {
             // The result can be accessed through the `m`-variable.
             message.reply("The streamer role is given to users featured in a Gamers React compilation.\nIf you have been featured, open a General Support <#858354762855874560> and leave a message with a timestamp and link to the video.");
