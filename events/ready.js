88f6e9471163730c0232ace74149791dd0cb6d60
RM20 <roshanhp209@gmail.com>
2022-07-15 16:59:36 +0100

    fixed unmute checker

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,11 @@
+const mutecheck = require('../automod/ontimer/mutecheck.js');
+const bancheck = require('../automod/ontimer/bancheck.js');
+
 module.exports = {
   name: "ready",
   execute(client) {
     console.log(`Logged In as ${client.user.tag}`);
+    mutecheck.mutechecker(client);
+    bancheck.backcheck(client);
   },
 };
